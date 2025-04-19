1dcbe457be139884ddaa4a5f90d08f6fc32f9452
jipenalver <jianchris2k14@gmail.com>
2025-04-20 01:01:32 +0800

    feat: add HeaderPanel component with props for dynamic header items and styling

---
+++ src/components/common/HeaderPanel.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  headerItems: string[]
+  headerIcon: string
+  headline?: string
+  image?: string
+}>()
+
+const items = computed(() =>
+  props.headerItems.map((item, index) => {
+    if (index === props.headerItems.length - 1) return { title: item, disabled: false }
+    return { title: item, disabled: true }
+  }),
+)
+</script>
+
+<template>
+  <v-card class="mb-5" :image="props.image">
+    <template #title>
+      <span class="text-h6 font-weight-bold">
+        <v-breadcrumbs :items="items">
+          <template #prepend>
+            <v-icon :icon="props.headerIcon" size="small" class="me-1"></v-icon>
+          </template>
+        </v-breadcrumbs>
+      </span>
+    </template>
+
+    <template #subtitle>
+      <p class="ms-4 text-wrap">{{ props.headline }}</p>
+    </template>
+  </v-card>
+</template>
