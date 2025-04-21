3631b2341662ee909022bba0ffce93dd23867b17
jipenalver <jianchris2k14@gmail.com>
2025-04-21 12:37:55 +0800

    feat: implement routing with initial login view and route definitions

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { routes } from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  routes,
+})
+
+router.beforeEach((to) => {
+  // // Use Pinia Store
+  // const authUserStore = useAuthUserStore()
+  // // Load if user is logged in
+  // const isLoggedIn = authUserStore.isAuthenticated()
+  // // If logged in, prevent access to login or register pages
+  // if (isLoggedIn && (to.name === 'home' || to.name === 'login' || to.name === 'register')) {
+  //   // redirect the user to the dashboard page
+  //   return { name: 'dashboard' }
+  // }
+  // // If not logged in, prevent access to system pages
+  // if (!isLoggedIn && to.meta.requiresAuth) {
+  //   // redirect the user to the login page
+  //   return { name: 'login' }
+  // }
+  // // Check if the user is logged in
+  // if (isLoggedIn) {
+  //   // Check if user role is User
+  //   if (authUserStore.userAccount) {
+  //     // Forbid access to admin pages
+  //     if (to.path.includes('/admin')) return { name: 'forbidden' }
+  //     // Check if user is not verified
+  //     if (!authUserStore.userData?.email_verified_at && to.name !== 'verify-email') {
+  //       // Redirect to verify email page
+  //       return { name: 'verify-email' }
+  //     }
+  //   }
+  //   // Check if user role is not Super Administrator
+  //   else if (!authUserStore.userAdmin) {
+  //     // Check page that is going to if it is in role pages
+  //     const isAccessible = authUserStore.authPages.includes(to.path)
+  //     // Forbid access if not in role pages and if page is not default page
+  //     if (!isAccessible && !to.meta.isDefault) return { name: 'forbidden' }
+  //   }
+  // }
 })
 
 export default router
---
+++ src/router/routes.ts
@@ -0,0 +1,71 @@
+import LoginView from '@/views/auth/LoginView.vue'
+
+export const routes = [
+  // Landing
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    meta: { requiresAuth: false },
+  },
+  // {
+  //   path: '/register',
+  //   name: 'register',
+  //   component: RegisterView,
+  //   meta: { requiresAuth: false }
+  // },
+  // {
+  //   path: '/password/forgot',
+  //   name: 'password-forgot',
+  //   component: ForgotPasswordView,
+  //   meta: { requiresAuth: false }
+  // },
+  // {
+  //   path: '/password/reset/:token',
+  //   name: 'password-reset',
+  //   component: ResetPasswordView,
+  //   meta: { requiresAuth: false }
+  // },
+
+  // System
+  // {
+  //   path: '/dashboard',
+  //   name: 'dashboard',
+  //   component: DashboardView,
+  //   meta: { requiresAuth: true, isDefault: true }
+  // },
+  // {
+  //   path: '/settings/:tab?',
+  //   name: 'settings',
+  //   component: SettingsView,
+  //   meta: { requiresAuth: true, isDefault: true }
+  // },
+
+  // Users
+  // {
+  //   path: '/admin/users/roles',
+  //   name: 'admin-users-roles',
+  //   component: UserRolesView,
+  //   meta: { requiresAuth: true }
+  // },
+  // {
+  //   path: '/admin/users/list',
+  //   name: 'admin-users-list',
+  //   component: UsersView,
+  //   meta: { requiresAuth: true }
+  // },
+
+  // Errors Pages
+  // {
+  //   path: '/forbidden',
+  //   name: 'forbidden',
+  //   component: ForbiddenView,
+  //   meta: { isDefault: true },
+  // },
+  // {
+  //   path: '/:catchAll(.*)',
+  //   name: 'not-found',
+  //   component: NotFoundView,
+  //   meta: { isDefault: true },
+  // },
+]
