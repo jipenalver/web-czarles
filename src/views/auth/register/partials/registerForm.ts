ac4ab439cb9ffcf51bd0e50fdeba83e9c5d70ea0
jipenalver <jianchris2k14@gmail.com>
2025-04-22 18:26:59 +0800

    feat: implement registration form using Supabase for user sign-up

---
+++ src/views/auth/register/partials/registerForm.ts
@@ -0,0 +1,65 @@
+import { formActionDefault } from '@/utils/helpers/constants'
+import { supabase } from '@/utils/supabase'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+export function useRegisterForm() {
+  const router = useRouter()
+
+  // States
+  const formDataDefault = {
+    email: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+  }
+  const formData = ref({ ...formDataDefault })
+  const formAction = ref({ ...formActionDefault })
+  const refVForm = ref()
+
+  // Actions
+  const onSubmit = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    const { email, password, ...otherData } = formData.value
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: otherData,
+      },
+    })
+
+    if (error) {
+      formAction.value = {
+        ...formActionDefault,
+        formMessage: error.message,
+        formStatus: error.status as number,
+        formAlert: true,
+      }
+    } else if (data) {
+      formAction.value = {
+        ...formActionDefault,
+        formMessage: 'Successfully Created Account.',
+        formStatus: 200,
+        formAlert: true,
+      }
+
+      router.push('/login')
+    }
+
+    refVForm.value?.reset()
+
+    formAction.value.formProcess = false
+  }
+
+  // Trigger Validators
+  const onFormSubmit = async () => {
+    const { valid } = await refVForm.value.validate()
+    if (valid) onSubmit()
+  }
+
+  // Expose State and Actions
+  return { formData, formAction, refVForm, onFormSubmit }
+}
