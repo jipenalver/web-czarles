fc5ee2788b1675b6175d228f3d157be081f5811b
jipenalver <jianchris2k14@gmail.com>
2025-04-24 09:41:35 +0800

    feat: add DashboardView component with layout and responsive drawer visibility

---
+++ src/views/system/dashboard/DashboardView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// import AnnouncementWidget from './partials/AnnouncementWidget.vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+// import WelcomeWidget from './partials/WelcomeWidget.vue'
+import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
+
+const { xs } = useDisplay()
+
+const isDrawerVisible = ref(xs.value ? false : true)
+</script>
+
+<template>
+  <AppLayout :is-with-app-bar-icon="true" @is-drawer-visible="isDrawerVisible = !isDrawerVisible">
+    <template #navigation>
+      <!-- <SideNavigation v-model:is-drawer-visible="isDrawerVisible"></SideNavigation> -->
+    </template>
+
+    <template #content>
+      <v-container fluid>
+        <v-row>
+          <!-- <AnnouncementWidget></AnnouncementWidget> -->
+
+          <v-col cols="12" lg="9">
+            <!-- <WelcomeWidget></WelcomeWidget> -->
+          </v-col>
+
+          <v-col cols="12" lg="3"> </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
