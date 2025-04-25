f119086cbb8c3e668d1d6961f33ca4ee49819a9c
jipenalver <jianchris2k14@gmail.com>
2025-04-25 18:40:48 +0800

    feat: add UsersView component for user management interface

---
+++ src/views/system/admin/manage-users/UsersView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
+import HeaderPanel from '@/components/common/HeaderPanel.vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import UsersTable from './partials/UsersTable.vue'
+import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
+
+const { xs } = useDisplay()
+
+const isDrawerVisible = ref(xs.value ? false : true)
+</script>
+
+<template>
+  <AppLayout :is-with-app-bar-icon="true" @is-drawer-visible="isDrawerVisible = !isDrawerVisible">
+    <template #navigation>
+      <SideNavigation v-model:is-drawer-visible="isDrawerVisible"></SideNavigation>
+    </template>
+
+    <template #content>
+      <v-container fluid>
+        <HeaderPanel
+          :header-items="['Users Management', 'List of Users']"
+          header-icon="mdi-list-box"
+          headline="Manage user accounts and assign roles dynamically."
+        ></HeaderPanel>
+
+        <UsersTable></UsersTable>
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
