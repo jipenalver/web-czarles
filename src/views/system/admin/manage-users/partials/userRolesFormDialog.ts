5d5c3e7ae9e3e132b6b1b0d3290873fcf256ee2b
jipenalver <jianchris2k14@gmail.com>
2025-04-27 16:20:28 +0800

    feat: implement useUserRolesFormDialog for user role management dialog

---
+++ src/views/system/admin/manage-users/partials/userRolesFormDialog.ts
@@ -0,0 +1,88 @@
+import { type UserRole, useUserRolesStore } from '@/stores/userRoles'
+import { formActionDefault } from '@/utils/helpers/constants'
+import { ref, watch } from 'vue'
+
+export function useUserRolesFormDialog(
+  props: {
+    isDialogVisible: boolean
+    itemData: UserRole | null
+  },
+  emit: (event: 'update:isDialogVisible', value: boolean) => void,
+) {
+  const userRolesStore = useUserRolesStore()
+
+  // States
+  const formDataDefault = {
+    user_role: '',
+    pages: [],
+  }
+  const formData = ref<Partial<UserRole>>({ ...formDataDefault })
+  const formAction = ref({ ...formActionDefault })
+  const refVForm = ref()
+  const isUpdate = ref(false)
+
+  watch(
+    () => props.isDialogVisible,
+    () => {
+      isUpdate.value = props.itemData ? true : false
+      formData.value = props.itemData
+        ? {
+            ...props.itemData,
+            pages: props.itemData.user_role_pages.map((item) => item.page),
+          }
+        : { ...formDataDefault }
+    },
+  )
+
+  // Actions
+  const onSubmit = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    const { data, error } = isUpdate.value
+      ? await userRolesStore.updateUserRole(formData.value)
+      : await userRolesStore.addUserRole(formData.value)
+
+    if (error) {
+      formAction.value = {
+        formMessage: error.message,
+        formStatus: 400,
+        formAlert: true,
+        formProcess: false,
+      }
+    } else if (data) {
+      formAction.value = {
+        ...formActionDefault,
+        formMessage: `Successfully ${isUpdate.value ? 'Updated' : 'Added'} User Role and its Pages.`,
+        formAlert: true,
+      }
+
+      await userRolesStore.getUserRoles()
+
+      setTimeout(() => {
+        onFormReset()
+      }, 1500)
+    }
+  }
+
+  // Trigger Validators
+  const onFormSubmit = async () => {
+    const { valid } = await refVForm.value.validate()
+    if (valid) onSubmit()
+  }
+
+  const onFormReset = () => {
+    formAction.value = { ...formActionDefault }
+    formData.value = { ...formDataDefault }
+    emit('update:isDialogVisible', false)
+  }
+
+  // Expose State and Actions
+  return {
+    formData,
+    formAction,
+    refVForm,
+    isUpdate,
+    onFormSubmit,
+    onFormReset,
+  }
+}
