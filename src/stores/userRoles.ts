1d21d887cd67c662494cdb9315b51928ce4e4804
jipenalver <jianchris2k14@gmail.com>
2025-04-27 16:20:20 +0800

    feat: add addUserRole and updateUserRole functions for user role management

--- src/stores/userRoles.ts
+++ src/stores/userRoles.ts
@@ -6,7 +6,7 @@ export type UserRole = {
   id: number
   user_role: string
   user_role_pages: { page: string }[]
-  [key: string]: unknown
+  pages: string[]
 }
 
 export const useUserRolesStore = defineStore('userRoles', () => {
@@ -23,6 +23,51 @@ export const useUserRolesStore = defineStore('userRoles', () => {
     userRoles.value = data as UserRole[]
   }
 
+  // Add User Roles
+  async function addUserRole(formData: Partial<UserRole>) {
+    const { pages, ...roleData } = formData
+
+    const { data, error } = await supabase.from('user_roles').insert(roleData).select()
+
+    if (data) await updateUserRolePages(data[0].id, pages as string[])
+
+    return { data, error }
+  }
+
+  // Update User Roles
+  async function updateUserRole(formData: Partial<UserRole>) {
+    const { pages, ...roleData } = formData
+
+    const { data, error } = await supabase
+      .from('user_roles')
+      .update(roleData)
+      .eq('id', roleData.id)
+      .select()
+
+    await updateUserRolePages(formData.id as number, pages as string[])
+
+    return { data, error }
+  }
+
+  // Update User Roles Pages
+  async function updateUserRolePages(id: number, pages: string[]) {
+    const { error: deleteError } = await supabase
+      .from('user_role_pages')
+      .delete()
+      .eq('user_role_id', id)
+
+    if (deleteError) return { error: deleteError }
+
+    const pageData = pages.map((page) => ({ page, user_role_id: id }))
+
+    const { data, error: insertError } = await supabase
+      .from('user_role_pages')
+      .insert(pageData)
+      .select()
+
+    return { data, error: insertError }
+  }
+
   // Expose States and Actions
-  return { userRoles, getUserRoles }
+  return { userRoles, getUserRoles, addUserRole, updateUserRole }
 })
