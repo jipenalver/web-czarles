adaf50ef25617edbdddb828b0d4e66627a54f57a
jipenalver <jianchris2k14@gmail.com>
2025-04-29 17:37:25 +0800

    feat: add UserCountWidget component to display total user count

---
+++ src/views/system/dashboard/partials/UserCountWidget.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-card elevation="8">
+    <template #prepend>
+      <v-chip color="secondary">
+        <v-icon icon="mdi-account-multiple"></v-icon>
+      </v-chip>
+    </template>
+
+    <v-card-text class="text-center">
+      <h2 class="text-h3">99</h2>
+      <p class="mb-3">Total Users</p>
+
+      <v-chip class="font-weight-bold" density="compact" color="secondary"> System Wide </v-chip>
+    </v-card-text>
+  </v-card>
+</template>
