1d5306adfc8afbeaf6bf445ef9c2b8a9ceb19815
jipenalver <jianchris2k14@gmail.com>
2025-06-14 20:56:53 +0800

    feat: implement useEmployeesTable for managing employee data and actions

---
+++ src/views/system/admin/manage-employees/employees/employeesTable.ts
@@ -0,0 +1,81 @@
+import { type Employee, useEmployeesStore } from '@/stores/employees'
+import { formActionDefault } from '@/utils/helpers/constants'
+import { type TableOptions } from '@/utils/helpers/tables'
+import { ref } from 'vue'
+
+export function useEmployeesTable() {
+  const employeesStore = useEmployeesStore()
+
+  // States
+  const tableOptions = ref({
+    page: 1,
+    itemsPerPage: 10,
+    sortBy: [],
+    isLoading: false,
+  })
+  const tableFilters = ref({
+    search: '',
+  })
+  const isDialogVisible = ref(false)
+  const isConfirmDeleteDialog = ref(false)
+  const deleteId = ref<number>(0)
+  const itemData = ref<Employee | null>(null)
+  const formAction = ref({ ...formActionDefault })
+
+  // Actions
+  const onAdd = () => {
+    itemData.value = null
+    isDialogVisible.value = true
+  }
+
+  const onUpdate = (item: Employee) => {
+    itemData.value = item
+    isDialogVisible.value = true
+  }
+
+  const onDelete = (id: number) => {
+    deleteId.value = id
+    isConfirmDeleteDialog.value = true
+  }
+
+  const onConfirmDelete = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    // const { data, error } = await employeesStore.deleteEmployee(deleteId.value)
+
+    // if (error) {
+    //   formAction.value.formMessage = error.message
+    //   formAction.value.formStatus = 400
+    // } else if (data) {
+    //   formAction.value.formMessage = 'Successfully Deleted Employee.'
+
+    //   await onLoadItems(tableOptions.value)
+    // }
+
+    formAction.value.formAlert = true
+    formAction.value.formProcess = false
+  }
+
+  const onLoadItems = async ({ page, itemsPerPage, sortBy }: TableOptions) => {
+    tableOptions.value.isLoading = true
+
+    await employeesStore.getEmployeesTable({ page, itemsPerPage, sortBy }, tableFilters.value)
+
+    tableOptions.value.isLoading = false
+  }
+
+  // Expose State and Actions
+  return {
+    tableOptions,
+    isDialogVisible,
+    isConfirmDeleteDialog,
+    itemData,
+    formAction,
+    onAdd,
+    onUpdate,
+    onDelete,
+    onConfirmDelete,
+    onLoadItems,
+    employeesStore,
+  }
+}
