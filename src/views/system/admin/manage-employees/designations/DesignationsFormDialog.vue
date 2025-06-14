8ec8204aaf61e3bbec6dcc8cfdbfd2f27809a86c
jipenalver <jianchris2k14@gmail.com>
2025-06-14 23:46:17 +0800

    feat: add DesignationsFormDialog component for managing designation details with form validation

---
+++ src/views/system/admin/manage-employees/designations/DesignationsFormDialog.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { useDesignationsFormDialog } from './designationsFormDialog'
+import { type TableOptions } from '@/utils/helpers/tables'
+import { type Designation } from '@/stores/designations'
+import AppAlert from '@/components/common/AppAlert.vue'
+import { requiredValidator } from '@/utils/validators'
+import { useDisplay } from 'vuetify'
+
+const props = defineProps<{
+  isDialogVisible: boolean
+  itemData: Designation | null
+  tableOptions: TableOptions
+  tableFilters: { search: string }
+}>()
+
+const emit = defineEmits(['update:isDialogVisible'])
+
+const { mdAndDown } = useDisplay()
+
+const { formData, formAction, refVForm, isUpdate, onFormSubmit, onFormReset } =
+  useDesignationsFormDialog(props, emit)
+</script>
+
+<template>
+  <AppAlert
+    v-model:is-alert-visible="formAction.formAlert"
+    :form-message="formAction.formMessage"
+    :form-status="formAction.formStatus"
+  ></AppAlert>
+
+  <v-dialog
+    :max-width="mdAndDown ? undefined : '600'"
+    :model-value="props.isDialogVisible"
+    :fullscreen="mdAndDown"
+    persistent
+  >
+    <v-card prepend-icon="mdi-tag" title="Designation Details">
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.designation"
+                label="Designation"
+                :rules="[requiredValidator]"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-textarea v-model="formData.description" label="Description"></v-textarea>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="pa-4">
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close" variant="plain" prepend-icon="mdi-close" @click="onFormReset"></v-btn>
+
+          <v-btn
+            prepend-icon="mdi-pencil"
+            color="primary"
+            type="submit"
+            variant="elevated"
+            :disabled="formAction.formProcess"
+            :loading="formAction.formProcess"
+          >
+            {{ isUpdate ? 'Update Designation' : 'Add Designation' }}
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
