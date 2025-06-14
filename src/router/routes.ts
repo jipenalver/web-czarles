61445759d58c47204776124d1ffeac638bab08d0
jipenalver <jianchris2k14@gmail.com>
2025-06-14 22:45:20 +0800

    feat: add Designations navigation and route for managing designations

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import LoginView from '@/views/auth/login/LoginView.vue'
 // import RegisterView from '@/views/auth/register/RegisterView.vue'
 import ForbiddenView from '@/views/errors/ForbiddenView.vue'
 import NotFoundView from '@/views/errors/NotFoundView.vue'
+import DesignationsView from '@/views/system/admin/manage-employees/DesignationsView.vue'
 import EmployeesView from '@/views/system/admin/manage-employees/EmployeesView.vue'
 import UserRolesView from '@/views/system/admin/manage-users/UserRolesView.vue'
 import UsersView from '@/views/system/admin/manage-users/UsersView.vue'
@@ -52,6 +53,12 @@ export const routes = [
   },
 
   // Employees
+  {
+    path: '/hrms/designations/list',
+    name: 'hrms-designations-list',
+    component: DesignationsView,
+    meta: { requiresAuth: true },
+  },
   {
     path: '/hrms/employees/list',
     name: 'hrms-employees-list',
