5f46fa30512afa07d4acbbe26d934c4ddf41f719
jipenalver <jianchris2k14@gmail.com>
2025-06-14 22:38:32 +0800

    feat: implement EmployeesTable component with search and onboarding functionality

---
+++ src/views/system/admin/manage-employees/employees/EmployeesTable.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts">
+import { type TableHeader } from '@/utils/helpers/tables'
+import AppAlert from '@/components/common/AppAlert.vue'
+import { useEmployeesTable } from './employeesTable'
+import { useDisplay } from 'vuetify'
+import { useDate } from 'vuetify'
+
+const date = useDate()
+const { mobile } = useDisplay()
+
+const tableHeaders: TableHeader[] = [
+  {
+    title: 'Email',
+    key: 'email',
+    align: 'start',
+  },
+  {
+    title: 'Fullname',
+    key: 'lastname',
+    align: 'start',
+  },
+  {
+    title: 'Phone',
+    key: 'phone',
+    align: 'start',
+  },
+  {
+    title: 'Designation',
+    key: 'designation',
+    sortable: false,
+    align: 'start',
+  },
+  {
+    title: 'Onboard Date',
+    key: 'created_at',
+    align: 'center',
+  },
+  {
+    title: 'Actions',
+    key: 'actions',
+    sortable: false,
+    align: 'center',
+  },
+]
+
+const {
+  tableOptions,
+  tableFilters,
+  isDialogVisible,
+  isConfirmDeleteDialog,
+  itemData,
+  formAction,
+  onAdd,
+  onUpdate,
+  onDelete,
+  onConfirmDelete,
+  onSearchItems,
+  onLoadItems,
+  employeesStore,
+} = useEmployeesTable()
+</script>
+
+<template>
+  <AppAlert
+    v-model:is-alert-visible="formAction.formAlert"
+    :form-message="formAction.formMessage"
+    :form-status="formAction.formStatus"
+  ></AppAlert>
+
+  <v-card>
+    <v-card-text>
+      <!-- eslint-disable vue/valid-v-slot -->
+      <v-data-table-server
+        v-model:items-per-page="tableOptions.itemsPerPage"
+        v-model:page="tableOptions.page"
+        v-model:sort-by="tableOptions.sortBy"
+        :loading="tableOptions.isLoading"
+        :headers="tableHeaders"
+        :items="employeesStore.employeesTable"
+        :items-length="employeesStore.employeesTableTotal"
+        @update:options="onLoadItems"
+        :hide-default-header="mobile"
+        :mobile="mobile"
+      >
+        <template #top>
+          <v-row dense>
+            <v-spacer></v-spacer>
+
+            <v-col cols="12" sm="5">
+              <v-text-field
+                v-model="tableFilters.search"
+                density="compact"
+                prepend-inner-icon="mdi-magnify"
+                placeholder="Search Firstname, Lastname, Email"
+                clearable
+                @click:clear="onSearchItems"
+                @input="onSearchItems"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="3">
+              <v-btn
+                class="my-1"
+                prepend-icon="mdi-account-plus"
+                color="primary"
+                block
+                @click="onAdd"
+              >
+                Onboard Employee
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-divider class="my-5"></v-divider>
+        </template>
+
+        <template #item.lastname="{ item }">
+          <span class="font-weight-bold"> {{ item.lastname }}, {{ item.firstname }} </span>
+        </template>
+
+        <template #item.phone="{ item }">
+          {{ item.phone ? '+63 ' + item.phone : '' }}
+        </template>
+
+        <template #item.designations="{ item }">
+          {{ item.designations.designation }}
+        </template>
+
+        <template #item.created_at="{ item }">
+          <span class="font-weight-bold">
+            {{ date.format(item.created_at, 'fullDateTime') }}
+          </span>
+        </template>
+
+        <template #item.actions="{ item }">
+          <div class="d-flex align-center" :class="mobile ? 'justify-end' : 'justify-center'">
+            <v-btn variant="text" density="comfortable" @click="onUpdate(item)" icon>
+              <v-icon icon="mdi-pencil"></v-icon>
+              <v-tooltip activator="parent" location="top">Edit Employee</v-tooltip>
+            </v-btn>
+
+            <v-btn variant="text" density="comfortable" @click="onDelete(item.id)" icon>
+              <v-icon icon="mdi-trash-can" color="primary"></v-icon>
+              <v-tooltip activator="parent" location="top">Delete Employee</v-tooltip>
+            </v-btn>
+          </div>
+        </template>
+      </v-data-table-server>
+    </v-card-text>
+  </v-card>
+</template>
