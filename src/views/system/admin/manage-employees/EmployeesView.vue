ce1b80baddd2f893c89f7bca478303e40ca3ddfd
jipenalver <jianchris2k14@gmail.com>
2025-06-15 23:43:04 +0800

    feat: implement tab navigation for managing employees, designations, and areas

--- src/views/system/admin/manage-employees/EmployeesView.vue
+++ src/views/system/admin/manage-employees/EmployeesView.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
+import DesignationsTable from './designations/DesignationsTable.vue'
 import HeaderPanel from '@/components/common/HeaderPanel.vue'
 import EmployeesTable from './employees/EmployeesTable.vue'
 import AppLayout from '@/components/layout/AppLayout.vue'
+import AreasTable from './areas/AreasTable.vue'
 import { useDisplay } from 'vuetify'
 import { ref } from 'vue'
 
 const { xs } = useDisplay()
 
+const tabsItems = [
+  {
+    icon: 'mdi-account-multiple',
+    text: 'Employees',
+    value: 'employees',
+  },
+  {
+    icon: 'mdi-account-tag',
+    text: 'Designations',
+    value: 'designations',
+  },
+  {
+    icon: 'mdi-map-marker',
+    text: 'Areas',
+    value: 'areas',
+  },
+]
+
+const tabWindow = ref('employees')
 const isDrawerVisible = ref(xs.value ? false : true)
 </script>
 
@@ -25,7 +46,34 @@ const isDrawerVisible = ref(xs.value ? false : true)
           headline="Onboard and manage employees."
         ></HeaderPanel>
 
-        <EmployeesTable component-view="employees"></EmployeesTable>
+        <v-tabs v-model="tabWindow" class="mb-5">
+          <v-tab
+            v-for="item in tabsItems"
+            :key="item.value"
+            class="mx-1"
+            :prepend-icon="item.icon"
+            :text="item.text"
+            :value="item.value"
+            min-width="200"
+            color="primary"
+            variant="flat"
+            rounded="lg"
+          ></v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tabWindow">
+          <v-tabs-window-item value="employees">
+            <EmployeesTable component-view="employees"></EmployeesTable>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="designations">
+            <DesignationsTable></DesignationsTable>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="areas">
+            <AreasTable></AreasTable>
+          </v-tabs-window-item>
+        </v-tabs-window>
       </v-container>
     </template>
   </AppLayout>
