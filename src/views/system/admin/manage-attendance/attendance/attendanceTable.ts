8f92ea55899d5c87a3dd1f91064df145932f9d26
jipenalver <jianchris2k14@gmail.com>
2025-06-16 02:30:10 +0800

    feat: add useDesignationsTable composable for managing attendance table state and actions

---
+++ src/views/system/admin/manage-attendance/attendance/attendanceTable.ts
@@ -0,0 +1,61 @@
+import { type Attendance, useAttendanceStore } from '@/stores/attendances'
+import { formActionDefault } from '@/utils/helpers/constants'
+import { type TableOptions } from '@/utils/helpers/tables'
+import { ref } from 'vue'
+
+export function useDesignationsTable() {
+  const attendanceStore = useAttendanceStore()
+
+  // States
+  const tableOptions = ref({
+    page: 1,
+    itemsPerPage: 10,
+    sortBy: [],
+    isLoading: false,
+  })
+  const tableFilters = ref({
+    employee_id: null,
+  })
+  const isDialogVisible = ref(false)
+  const isConfirmDeleteDialog = ref(false)
+  const itemData = ref<Attendance | null>(null)
+  const formAction = ref({ ...formActionDefault })
+
+  // Actions
+  const onAdd = () => {
+    itemData.value = null
+    isDialogVisible.value = true
+  }
+
+  const onUpdate = (item: Attendance) => {
+    itemData.value = item
+    isDialogVisible.value = true
+  }
+
+  const onFilterItems = () => {
+    onLoadItems(tableOptions.value)
+  }
+
+  const onLoadItems = async ({ page, itemsPerPage, sortBy }: TableOptions) => {
+    tableOptions.value.isLoading = true
+
+    await attendanceStore.getAttendancesTable({ page, itemsPerPage, sortBy }, tableFilters.value)
+
+    tableOptions.value.isLoading = false
+  }
+
+  // Expose State and Actions
+  return {
+    tableOptions,
+    tableFilters,
+    isDialogVisible,
+    isConfirmDeleteDialog,
+    itemData,
+    formAction,
+    onAdd,
+    onUpdate,
+    onFilterItems,
+    onLoadItems,
+    attendanceStore,
+  }
+}
