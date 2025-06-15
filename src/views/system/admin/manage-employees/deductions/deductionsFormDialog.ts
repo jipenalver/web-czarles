b5b33a4b3c38c7076bea87791f8c6b343b807c41
jipenalver <jianchris2k14@gmail.com>
2025-06-16 01:45:44 +0800

    feat: implement deductions form dialog functionality with state management

---
+++ src/views/system/admin/manage-employees/deductions/deductionsFormDialog.ts
@@ -0,0 +1,82 @@
+import {
+  type EmployeeTableFilter,
+  // useEmployeesStore
+} from '@/stores/employees'
+import { formActionDefault } from '@/utils/helpers/constants'
+import { type TableOptions } from '@/utils/helpers/tables'
+import { useBenefitsStore } from '@/stores/benefits'
+import { onMounted, ref } from 'vue'
+
+export function useDeductionsFormDialog(
+  props: {
+    isDialogVisible: boolean
+    itemId: number
+    tableOptions: TableOptions
+    tableFilters: EmployeeTableFilter
+  },
+  emit: (event: 'update:isDialogVisible', value: boolean) => void,
+) {
+  // const employeesStore = useEmployeesStore()
+  const benefitsStore = useBenefitsStore()
+
+  // States
+  const formDataDefault = {
+    amount: false,
+    daily_rate: undefined,
+  }
+  const formData = ref({ ...formDataDefault })
+  const formAction = ref({ ...formActionDefault })
+  const refVForm = ref()
+
+  // Actions
+  const onSubmit = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    // const { data, error } = await employeesStore.updateEmployee(formData.value)
+
+    // if (error) {
+    //   formAction.value = {
+    //     ...formActionDefault,
+    //     formMessage: error.message,
+    //     formStatus: 400,
+    //     formProcess: false,
+    //   }
+    // } else if (data) {
+    //   formAction.value.formMessage = `Successfully Updated Employee Deduction(s).`
+
+    //   await employeesStore.getEmployeesTable(props.tableOptions, props.tableFilters)
+
+    //   setTimeout(() => {
+    //     onFormReset()
+    //   }, 1500)
+    // }
+
+    formAction.value.formAlert = true
+  }
+
+  // Trigger Validators
+  const onFormSubmit = async () => {
+    const { valid } = await refVForm.value.validate()
+    if (valid) onSubmit()
+  }
+
+  const onFormReset = () => {
+    formAction.value = { ...formActionDefault }
+    formData.value = { ...formDataDefault }
+    emit('update:isDialogVisible', false)
+  }
+
+  onMounted(async () => {
+    if (benefitsStore.benefits.length === 0) await benefitsStore.getBenefits()
+  })
+
+  // Expose State and Actions
+  return {
+    formData,
+    formAction,
+    refVForm,
+    onFormSubmit,
+    onFormReset,
+    benefitsStore,
+  }
+}
