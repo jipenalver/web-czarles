3e0bcd79672bfc0f2539015adc9236aadb107f8d
jipenalver <jianchris2k14@gmail.com>
2025-06-16 09:42:09 +0800

    feat: add AttendanceExpandedRow component for attendance details display

---
+++ src/views/system/admin/manage-attendance/attendance/AttendanceExpandedRow.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { type Attendance } from '@/stores/attendances'
+// import { useDisplay } from 'vuetify'
+// import { useDate } from 'vuetify'
+
+const props = defineProps<{
+  columnsLength: number
+  itemData: Attendance
+}>()
+
+// const date = useDate()
+// const { mobile } = useDisplay()
+</script>
+
+<template>
+  <tr>
+    <td :colspan="props.columnsLength" class="py-2">
+      <v-row dense> </v-row>
+    </td>
+  </tr>
+</template>
