7dcaa9912d8bc790145f62786a4e3b8ecf5ae5a4
jipenalver <jianchris2k14@gmail.com>
2025-06-22 13:37:57 +0800

    feat: add AttendanceFormDialog component for managing attendance records

---
+++ src/views/system/admin/manage-attendance/attendance/AttendanceFormDialog.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { type Attendance, type AttendanceTableFilter } from '@/stores/attendances'
+import { useAttendanceFormDialog } from './attendanceFormDialog'
+import { type TableOptions } from '@/utils/helpers/tables'
+import AppAlert from '@/components/common/AppAlert.vue'
+import { requiredValidator } from '@/utils/validators'
+import { useDisplay } from 'vuetify'
+
+const props = defineProps<{
+  isDialogVisible: boolean
+  itemData: Attendance | null
+  tableOptions: TableOptions
+  tableFilters: AttendanceTableFilter
+}>()
+
+const emit = defineEmits(['update:isDialogVisible'])
+
+const { mdAndDown } = useDisplay()
+
+const { formData, formAction, refVForm, isUpdate, onFormSubmit, onFormReset, employeesStore } =
+  useAttendanceFormDialog(props, emit)
+</script>
+
+<template>
+  <AppAlert
+    v-model:is-alert-visible="formAction.formAlert"
+    :form-message="formAction.formMessage"
+    :form-status="formAction.formStatus"
+  ></AppAlert>
+
+  <v-dialog
+    :max-width="mdAndDown ? undefined : '600'"
+    :model-value="props.isDialogVisible"
+    :fullscreen="mdAndDown"
+    persistent
+  >
+    <v-card
+      prepend-icon="mdi-account-cash"
+      title="Attendance"
+      :subtitle="isUpdate ? 'Update Attendance Record' : 'Add Employee\'s Attendance Record'"
+    >
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12">
+              <v-autocomplete
+                v-model="formData.employee_id"
+                :items="employeesStore.employees"
+                label="Employee"
+                item-title="label"
+                item-value="id"
+                :rules="[requiredValidator]"
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="pa-4">
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close" variant="plain" prepend-icon="mdi-close" @click="onFormReset"></v-btn>
+
+          <v-btn
+            prepend-icon="mdi-pencil"
+            color="primary"
+            type="submit"
+            variant="elevated"
+            :disabled="formAction.formProcess"
+            :loading="formAction.formProcess"
+          >
+            {{ isUpdate ? 'Update Attendance' : 'Add Attendance' }}
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
