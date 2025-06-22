436ab7d0d4b4e0bedb3ca39022a30c8dc0db83f0
jipenalver <jianchris2k14@gmail.com>
2025-06-22 13:18:53 +0800

    feat: enhance useAttendanceFormDialog with improved state management and form handling

--- src/views/system/admin/manage-attendance/attendance/attendanceFormDialog.ts
+++ src/views/system/admin/manage-attendance/attendance/attendanceFormDialog.ts
@@ -1,4 +1,9 @@
-import { type Attendance, AttendanceTableFilter } from '@/stores/attendances'
+import {
+  type Attendance,
+  type AttendanceForm,
+  type AttendanceTableFilter,
+  useAttendancesStore,
+} from '@/stores/attendances'
 import { formActionDefault } from '@/utils/helpers/constants'
 import { type TableOptions } from '@/utils/helpers/tables'
 import { useEmployeesStore } from '@/stores/employees'
@@ -15,6 +20,72 @@ export function useAttendanceFormDialog(
 ) {
   const attendancesStore = useAttendancesStore()
   const employeesStore = useEmployeesStore()
+
+  // States
+  const formDataDefault = {
+    date: null,
+    am_time_in: '',
+    am_time_out: '',
+    pm_time_in: '',
+    pm_time_out: '',
+    employee_id: null,
+  }
+  const formData = ref<Partial<AttendanceForm>>({ ...formDataDefault })
+  const formAction = ref({ ...formActionDefault })
+  const refVForm = ref()
+  const isUpdate = ref(false)
+
+  watch(
+    () => props.isDialogVisible,
+    () => {
+      isUpdate.value = props.itemData ? true : false
+      formData.value = props.itemData ? { ...props.itemData } : { ...formDataDefault }
+    },
+  )
+
+  // Actions
+  const onSubmit = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    const { data, error } = isUpdate.value
+      ? await attendancesStore.updateAttendance(formData.value)
+      : await attendancesStore.addAttendance(formData.value)
+
+    if (error) {
+      formAction.value = {
+        ...formActionDefault,
+        formMessage: error.message,
+        formStatus: 400,
+        formProcess: false,
+      }
+    } else if (data) {
+      formAction.value.formMessage = `Successfully Added Attendance.`
+
+      await attendancesStore.getAttendancesTable(props.tableOptions, props.tableFilters)
+
+      setTimeout(() => {
+        onFormReset()
+      }, 1500)
+    }
+
+    formAction.value.formAlert = true
+  }
+
+  // Trigger Validators
+  const onFormSubmit = async () => {
+    const { valid } = await refVForm.value.validate()
+    if (valid) onSubmit()
+  }
+
+  const onFormReset = () => {
+    formAction.value = { ...formActionDefault }
+    formData.value = { ...formDataDefault }
+    emit('update:isDialogVisible', false)
+  }
+
+  onMounted(async () => {
+    if (employeesStore.employees.length === 0) await employeesStore.getEmployees()
+  })
 
   // Expose State and Actions
   return {
