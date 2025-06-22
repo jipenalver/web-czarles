c4f498a8a6454e13fd37fe6a2b64420726d787aa
jipenalver <jianchris2k14@gmail.com>
2025-06-22 14:57:49 +0800

    feat: enhance attendance form dialog to handle time formatting and duplicate attendance checks

--- src/views/system/admin/manage-attendance/attendance/attendanceFormDialog.ts
+++ src/views/system/admin/manage-attendance/attendance/attendanceFormDialog.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   type Attendance,
   type AttendanceForm,
   type AttendanceTableFilter,
   useAttendancesStore,
 } from '@/stores/attendances'
+import { getDate, getTime24Hour } from '@/utils/helpers/others'
 import { formActionDefault } from '@/utils/helpers/constants'
 import { type TableOptions } from '@/utils/helpers/tables'
 import { useEmployeesStore } from '@/stores/employees'
@@ -29,6 +31,7 @@ export function useAttendanceFormDialog(
     pm_time_in: '',
     pm_time_out: '',
     employee_id: null,
+    is_rectified: true,
   }
   const formData = ref<Partial<AttendanceForm>>({ ...formDataDefault })
   const formAction = ref({ ...formActionDefault })
@@ -39,7 +42,18 @@ export function useAttendanceFormDialog(
     () => props.isDialogVisible,
     () => {
       isUpdate.value = props.itemData ? true : false
-      formData.value = props.itemData ? { ...props.itemData } : { ...formDataDefault }
+
+      if (isUpdate.value) {
+        const { employee, ...itemData } = props.itemData as Attendance
+        formData.value = {
+          ...itemData,
+          am_time_in: getTime24Hour(itemData.am_time_in) as string,
+          am_time_out: getTime24Hour(itemData.am_time_out) as string,
+          pm_time_in: getTime24Hour(itemData.pm_time_in) as string,
+          pm_time_out: getTime24Hour(itemData.pm_time_out) as string,
+          date: getDate(itemData.am_time_in) as string,
+        }
+      } else formData.value = { ...formDataDefault }
     },
   )
 
@@ -47,9 +61,48 @@ export function useAttendanceFormDialog(
   const onSubmit = async () => {
     formAction.value = { ...formActionDefault, formProcess: true }
 
+    if (!isUpdate.value) {
+      const hasAttendance = attendancesStore.attendances.find((attendance) => {
+        const attendanceDate = new Date(formData.value.date as string)
+
+        if (
+          getDate(attendance.am_time_in) === getDate(attendanceDate) &&
+          attendance.employee_id === formData.value.employee_id
+        )
+          return true
+      })
+
+      if (hasAttendance) {
+        formAction.value = {
+          ...formActionDefault,
+          formMessage: 'Attendance for this date and employee already exists.',
+          formStatus: 400,
+          formProcess: false,
+          formAlert: true,
+        }
+        return
+      }
+    }
+
+    const { date, ...newFormData } = {
+      ...formData.value,
+      am_time_in: formData.value.am_time_in
+        ? formData.value.date + ' ' + formData.value.am_time_in
+        : null,
+      am_time_out: formData.value.am_time_out
+        ? formData.value.date + ' ' + formData.value.am_time_out
+        : null,
+      pm_time_in: formData.value.pm_time_in
+        ? formData.value.date + ' ' + formData.value.pm_time_in
+        : null,
+      pm_time_out: formData.value.pm_time_out
+        ? formData.value.date + ' ' + formData.value.pm_time_out
+        : null,
+    }
+
     const { data, error } = isUpdate.value
-      ? await attendancesStore.updateAttendance(formData.value)
-      : await attendancesStore.addAttendance(formData.value)
+      ? await attendancesStore.updateAttendance(newFormData)
+      : await attendancesStore.addAttendance(newFormData)
 
     if (error) {
       formAction.value = {
@@ -85,6 +138,7 @@ export function useAttendanceFormDialog(
 
   onMounted(async () => {
     if (employeesStore.employees.length === 0) await employeesStore.getEmployees()
+    if (attendancesStore.attendances.length === 0) await attendancesStore.getAttendances()
   })
 
   // Expose State and Actions
