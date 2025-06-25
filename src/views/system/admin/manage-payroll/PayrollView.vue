7385f6668c030a577fbe6102f45eaf279b46cbe2
jipenalver <jianchris2k14@gmail.com>
2025-06-25 18:05:23 +0800

    feat: add holidays tab and integrate HolidaysTable in PayrollView

--- src/views/system/admin/manage-payroll/PayrollView.vue
+++ src/views/system/admin/manage-payroll/PayrollView.vue
@@ -2,11 +2,26 @@
 import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
 import HeaderPanel from '@/components/common/HeaderPanel.vue'
 import AppLayout from '@/components/layout/AppLayout.vue'
+import HolidaysTable from './holidays/HolidaysTable.vue'
 import { useDisplay } from 'vuetify'
 import { ref } from 'vue'
 
 const { xs } = useDisplay()
 
+const tabsItems = [
+  {
+    icon: 'mdi-account-cash',
+    text: 'Payroll',
+    value: 'payroll',
+  },
+  {
+    icon: 'mdi-bed',
+    text: 'Holidays',
+    value: 'holidays',
+  },
+]
+
+const tabWindow = ref('payroll')
 const isDrawerVisible = ref(xs.value ? false : true)
 </script>
 
@@ -23,6 +38,29 @@ const isDrawerVisible = ref(xs.value ? false : true)
           header-icon="mdi-account-cash"
           headline="Manage employee's payroll."
         ></HeaderPanel>
+
+        <v-tabs v-model="tabWindow" class="mb-5">
+          <v-tab
+            v-for="item in tabsItems"
+            :key="item.value"
+            class="mx-1"
+            :prepend-icon="item.icon"
+            :text="item.text"
+            :value="item.value"
+            min-width="200"
+            color="primary"
+            variant="flat"
+            rounded="lg"
+          ></v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tabWindow">
+          <v-tabs-window-item value="payroll"> </v-tabs-window-item>
+
+          <v-tabs-window-item value="holidays">
+            <HolidaysTable></HolidaysTable>
+          </v-tabs-window-item>
+        </v-tabs-window>
       </v-container>
     </template>
   </AppLayout>
