add6abc8c7dd60b70edfd24e5e6f277baf66bc3a
jipenalver <jianchris2k14@gmail.com>
2025-07-01 12:58:17 +0800

    fix: adjust layout and add maxHeight prop for EmployeeLogs component

--- src/views/system/admin/manage-employees/employees/EmployeeLogs.vue
+++ src/views/system/admin/manage-employees/employees/EmployeeLogs.vue
@@ -7,6 +7,7 @@ import { onMounted } from 'vue'
 const props = defineProps<{
   itemId?: number
   type: 'rates' | 'benefits'
+  maxHeight?: string
 }>()
 
 const date = useDate()
@@ -19,38 +20,40 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h2 class="text-body-1 font-weight-black my-3 ms-2">Adjustment Logs</h2>
+  <h2 class="text-body-1 font-weight-black mb-3 ms-2">Adjustment Logs</h2>
 
-  <v-list lines="one" density="compact" nav>
-    <v-list-item v-for="log in logsStore.logsByEmployee" :key="log.id">
-      <template #prepend>
-        <v-avatar
-          v-if="log.user_avatar"
-          :image="log.user_avatar"
-          color="primary"
-          size="small"
-        ></v-avatar>
+  <div class="overflow-x-auto" :style="{ maxHeight: props.maxHeight || '300px' }">
+    <v-list lines="one" density="compact" nav>
+      <v-list-item v-for="log in logsStore.logsByEmployee" :key="log.id">
+        <template #prepend>
+          <v-avatar
+            v-if="log.user_avatar"
+            :image="log.user_avatar"
+            color="primary"
+            size="small"
+          ></v-avatar>
 
-        <v-avatar v-else color="primary" size="small">
-          <span class="text-caption">
-            {{ getAvatarText(log.user_fullname) }}
-          </span>
-        </v-avatar>
-      </template>
+          <v-avatar v-else color="primary" size="small">
+            <span class="text-caption">
+              {{ getAvatarText(log.user_fullname) }}
+            </span>
+          </v-avatar>
+        </template>
 
-      <v-list-item-title>
-        <p class="text-caption text-wrap">{{ log.description }}</p>
-      </v-list-item-title>
-      <v-list-item-subtitle>
-        {{ log.user_fullname }}
-      </v-list-item-subtitle>
+        <v-list-item-title>
+          <p class="text-caption text-wrap">{{ log.description }}</p>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ log.user_fullname }}
+        </v-list-item-subtitle>
 
-      <template #append>
-        <div class="d-flex flex-column align-end text-caption">
-          <span>{{ date.format(log.created_at, 'fullDate') }}</span>
-          <span>{{ date.format(log.created_at, 'fullTime') }}</span>
-        </div>
-      </template>
-    </v-list-item>
-  </v-list>
+        <template #append>
+          <div class="d-flex flex-column align-end text-caption">
+            <span>{{ date.format(log.created_at, 'fullDate') }}</span>
+            <span>{{ date.format(log.created_at, 'fullTime') }}</span>
+          </div>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
 </template>
