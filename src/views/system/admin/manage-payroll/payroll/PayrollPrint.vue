0f52a4c4abaffad225b1170accd951df355b1e24
jipenalver <jianchris2k14@gmail.com>
2025-07-03 21:40:16 +0800

    feat: update PayrollPrint layout with improved styling and add signature section

--- src/views/system/admin/manage-payroll/payroll/PayrollPrint.vue
+++ src/views/system/admin/manage-payroll/payroll/PayrollPrint.vue
@@ -8,28 +8,28 @@
     </v-col>
   </v-row>
 
-  <table class="mt-6 text-caption w-100">
+  <table class="mt-6 text-body-2 w-100">
     <tbody>
       <tr>
-        <td>PAID TO</td>
+        <td class="text-caption">PAID TO</td>
         <td class="border-b-md w-66 ps-5">Juan Dela Cruz</td>
-        <td>POSITION</td>
+        <td class="text-caption">POSITION</td>
         <td class="border-b-md w-25 ps-5">Software Engineer</td>
       </tr>
       <tr>
-        <td>ADDRESS</td>
+        <td class="text-caption">ADDRESS</td>
         <td class="border-b-md w-66 ps-5">Butuan City</td>
-        <td>DATE</td>
+        <td class="text-caption">DATE</td>
         <td class="border-b-md w-25 ps-5">February 26, 2025</td>
       </tr>
     </tbody>
   </table>
 
-  <table class="mt-3 text-caption border-md w-100" style="border-collapse: collapse">
+  <table class="mt-3 text-body-2 border-md border-collapse w-100">
     <tbody>
       <tr>
-        <td class="text-center border-b-md" colspan="4">PARTICULARS</td>
-        <td class="text-center border-b-md border-s-md">AMOUNT</td>
+        <td class="text-caption text-center border-b-md" colspan="4">PARTICULARS</td>
+        <td class="text-caption text-center border-b-md border-s-md">AMOUNT</td>
       </tr>
       <tr>
         <td class="border-b-sm text-center">5</td>
@@ -58,4 +58,32 @@
       </tr>
     </tbody>
   </table>
+
+  <v-row dense no-gutters>
+    <v-col cols="12" sm="3" class="d-flex justify-center align-center">
+      <table class="mt-3 text-caption border-md w-100">
+        <tbody>
+          <tr>
+            <td>Prepared by:</td>
+            <td class="border-b-sm"></td>
+            <td>Date:</td>
+            <td class="border-b-sm"></td>
+          </tr>
+          <tr>
+            <td>Approved by:</td>
+            <td class="border-b-sm"></td>
+            <td>Date:</td>
+            <td class="border-b-sm"></td>
+          </tr>
+        </tbody>
+      </table>
+    </v-col>
+    <v-col cols="12" sm="9" class="d-flex justify-center align-center"> </v-col>
+  </v-row>
 </template>
+
+<style scoped>
+.border-collapse {
+  border-collapse: collapse;
+}
+</style>
