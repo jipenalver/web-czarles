e2117bfe9cd06a25cb0f9d08700152e31afe08f6
jipenalver <jianchris2k14@gmail.com>
2025-07-03 23:19:32 +0800

    feat: add PayrollTableDialog component and usePayrollTableDialog for dialog management

---
+++ src/views/system/admin/manage-payroll/payroll/PayrollTableDialog.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { usePayrollTableDialog } from './payrollTableDialog'
+import AppAlert from '@/components/common/AppAlert.vue'
+import { useDisplay } from 'vuetify'
+
+const props = defineProps<{
+  isDialogVisible: boolean
+  itemId?: number
+}>()
+
+const emit = defineEmits(['update:isDialogVisible'])
+
+const { mdAndDown } = useDisplay()
+
+const {
+  formAction,
+  // onPreview,
+  onDialogClose,
+} = usePayrollTableDialog(props, emit)
+</script>
+
+<template>
+  <AppAlert
+    v-model:is-alert-visible="formAction.formAlert"
+    :form-message="formAction.formMessage"
+    :form-status="formAction.formStatus"
+  ></AppAlert>
+
+  <v-dialog
+    :max-width="mdAndDown ? undefined : '1200'"
+    :model-value="props.isDialogVisible"
+    :fullscreen="mdAndDown"
+    persistent
+  >
+    <v-card prepend-icon="mdi-account-cash" title="Payroll Preview">
+      <v-card-text> </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions class="pa-4">
+        <v-spacer></v-spacer>
+
+        <v-btn text="Close" variant="plain" prepend-icon="mdi-close" @click="onDialogClose"></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
