f9666f5cbc13555339a18cdb8a36ed19f9cc86d0
jipenalver <jianchris2k14@gmail.com>
2025-07-03 20:12:12 +0800

    feat: enhance Add-ons and Deductions form with is_quincena toggle and structured input handling

--- src/views/system/admin/manage-employees/addons-deductions/AddonsDeductionsFormDialog.vue
+++ src/views/system/admin/manage-employees/addons-deductions/AddonsDeductionsFormDialog.vue
@@ -50,31 +50,65 @@ const {
           <v-row>
             <v-col cols="12" sm="6">
               <v-row dense>
-                <h2 class="text-body-1 font-weight-black mb-3 ms-2">Add-ons</h2>
-
-                <v-col cols="12" v-for="(benefit, index) in benefitsStore.addons" :key="benefit.id">
-                  <v-text-field
-                    v-model="formAddons[index]"
-                    prepend-inner-icon="mdi-currency-php"
-                    :label="benefit.benefit"
-                    type="number"
-                  ></v-text-field>
+                <v-col cols="12">
+                  <h2 class="text-body-1 font-weight-black mb-3 ms-2">Add-ons</h2>
                 </v-col>
 
-                <h2 class="text-body-1 font-weight-black mb-3 ms-2">Deductions</h2>
+                <template v-for="(benefit, index) in benefitsStore.addons" :key="benefit.id">
+                  <v-col cols="12" sm="8">
+                    <v-text-field
+                      v-model="formAddons.amount[index]"
+                      prepend-inner-icon="mdi-currency-php"
+                      :label="benefit.benefit"
+                      type="number"
+                    ></v-text-field>
+                  </v-col>
 
-                <v-col
-                  cols="12"
-                  v-for="(benefit, index) in benefitsStore.deductions"
-                  :key="benefit.id"
-                >
-                  <v-text-field
-                    v-model="formDeductions[index]"
-                    prepend-inner-icon="mdi-currency-php"
-                    :label="benefit.benefit"
-                    type="number"
-                  ></v-text-field>
+                  <v-col cols="12" sm="4">
+                    <v-switch
+                      v-model="formAddons.is_quincena[index]"
+                      class="ms-2"
+                      color="primary"
+                      hide-details
+                    >
+                      <template #label>
+                        <span class="font-weight-black ms-1">
+                          {{ formAddons.is_quincena[index] ? 'Quincena' : 'Monthly' }}
+                        </span>
+                      </template>
+                    </v-switch>
+                  </v-col>
+                </template>
+
+                <v-col cols="12">
+                  <h2 class="text-body-1 font-weight-black mb-3 ms-2">Deductions</h2>
                 </v-col>
+
+                <template v-for="(benefit, index) in benefitsStore.deductions" :key="benefit.id">
+                  <v-col cols="12" sm="8">
+                    <v-text-field
+                      v-model="formDeductions.amount[index]"
+                      prepend-inner-icon="mdi-currency-php"
+                      :label="benefit.benefit"
+                      type="number"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" sm="4">
+                    <v-switch
+                      v-model="formDeductions.is_quincena[index]"
+                      class="ms-2"
+                      color="primary"
+                      hide-details
+                    >
+                      <template #label>
+                        <span class="font-weight-black ms-1">
+                          {{ formDeductions.is_quincena[index] ? 'Quincena' : 'Monthly' }}
+                        </span>
+                      </template>
+                    </v-switch>
+                  </v-col>
+                </template>
               </v-row>
             </v-col>
 
