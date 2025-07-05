dc9dd1de728b1bff6993c78b1e64916bc36d29df
jipenalver <jianchris2k14@gmail.com>
2025-07-05 22:46:35 +0800

    feat: extend viewType options in AttendanceViewDialog and simplify imageType assignment

--- src/views/system/admin/manage-attendance/attendance/attendanceViewDialog.ts
+++ src/views/system/admin/manage-attendance/attendance/attendanceViewDialog.ts
@@ -7,10 +7,25 @@ export function useAttendanceViewDialog(
   props: {
     isDialogVisible: boolean
     itemData: Attendance | null
-    viewType: 'am_time_in' | 'am_time_out' | 'pm_time_in' | 'pm_time_out'
+    viewType:
+      | 'am_time_in'
+      | 'am_time_out'
+      | 'pm_time_in'
+      | 'pm_time_out'
+      | 'overtime_in'
+      | 'overtime_out'
   },
   emit: (event: 'update:isDialogVisible', value: boolean) => void,
 ) {
+  const imageTypeMap = {
+    am_time_in: 'AM - Time In',
+    am_time_out: 'AM - Time Out',
+    pm_time_in: 'PM - Time In',
+    pm_time_out: 'PM - Time Out',
+    overtime_in: 'Overtime - Time In',
+    overtime_out: 'Overtime - Time Out',
+  }
+
   // States
   const formAction = ref({ ...formActionDefault })
   const imageType = ref('')
@@ -19,14 +34,7 @@ export function useAttendanceViewDialog(
   watch(
     () => props.isDialogVisible,
     () => {
-      imageType.value =
-        props.viewType === 'am_time_in'
-          ? 'AM - Time In'
-          : props.viewType === 'am_time_out'
-            ? 'AM - Time Out'
-            : props.viewType === 'pm_time_in'
-              ? 'PM - Time In'
-              : 'PM - Time Out'
+      imageType.value = imageTypeMap[props.viewType] || 'Unknown'
       imageData.value =
         props.itemData?.attendance_images.find((image) => image.image_type === props.viewType) ||
         null
