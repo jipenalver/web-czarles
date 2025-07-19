28b36736e1769bd82b235cfa0aa849ddc050b0e8
jipenalver <jianchris2k14@gmail.com>
2025-07-19 15:00:55 +0800

    feat: implement UnitsTable component for managing unit data and actions

---
+++ src/views/system/admin/manage-payroll/units/unitsTable.ts
@@ -0,0 +1,93 @@
+import { formActionDefault } from '@/utils/helpers/constants'
+import { type TableOptions } from '@/utils/helpers/tables'
+import { type Unit, useUnitsStore } from '@/stores/units'
+import { ref } from 'vue'
+
+export function useUnitsTable() {
+  const unitsStore = useUnitsStore()
+
+  // States
+  const tableOptions = ref({
+    page: 1,
+    itemsPerPage: 10,
+    sortBy: [],
+    isLoading: false,
+  })
+  const tableFilters = ref({
+    search: '',
+  })
+  const isDialogVisible = ref(false)
+  const isConfirmDeleteDialog = ref(false)
+  const deleteId = ref<number>(0)
+  const itemData = ref<Unit | null>(null)
+  const formAction = ref({ ...formActionDefault })
+
+  // Actions
+  const onAdd = () => {
+    itemData.value = null
+    isDialogVisible.value = true
+  }
+
+  const onUpdate = (item: Unit) => {
+    itemData.value = item
+    isDialogVisible.value = true
+  }
+
+  const onDelete = (id: number) => {
+    deleteId.value = id
+    isConfirmDeleteDialog.value = true
+  }
+
+  const onConfirmDelete = async () => {
+    formAction.value = { ...formActionDefault, formProcess: true }
+
+    const { data, error } = await unitsStore.deleteUnit(deleteId.value)
+
+    if (error) {
+      formAction.value.formMessage = error.message
+      formAction.value.formStatus = 400
+    } else if (data) {
+      formAction.value.formMessage = 'Successfully Deleted Unit.'
+
+      await onLoadItems(tableOptions.value)
+      await unitsStore.getUnits()
+    }
+
+    formAction.value.formAlert = true
+    formAction.value.formProcess = false
+  }
+
+  const onSearchItems = () => {
+    if (
+      tableFilters.value.search?.length >= 2 ||
+      tableFilters.value.search?.length == 0 ||
+      tableFilters.value.search === null
+    )
+      onLoadItems(tableOptions.value)
+  }
+
+  const onLoadItems = async ({ page, itemsPerPage, sortBy }: TableOptions) => {
+    tableOptions.value.isLoading = true
+
+    await unitsStore.getUnitsTable({ page, itemsPerPage, sortBy }, tableFilters.value)
+
+    tableOptions.value.isLoading = false
+  }
+
+  // Expose State and Actions
+  return {
+    tableOptions,
+    tableFilters,
+    isDialogVisible,
+    isConfirmDeleteDialog,
+    itemData,
+    formAction,
+    onAdd,
+    onUpdate,
+    onDelete,
+    onConfirmDelete,
+    onSearchItems,
+    onLoadItems,
+    unitsStore,
+  }
+}
