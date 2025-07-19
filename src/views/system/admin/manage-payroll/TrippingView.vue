19be54c4073fa69852a86b842c87de19d2192ffb
jipenalver <jianchris2k14@gmail.com>
2025-07-19 14:19:12 +0800

    feat: add TrippingView component for managing employee trips

---
+++ src/views/system/admin/manage-payroll/TrippingView.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import SideNavigation from '@/components/layout/navigation/SideNavigation.vue'
+import HeaderPanel from '@/components/common/HeaderPanel.vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
+
+const { xs } = useDisplay()
+
+const tabsItems = [
+  {
+    icon: 'mdi-highway',
+    text: 'Trippings',
+    value: 'trippings',
+  },
+  {
+    icon: 'mdi-dump-truck',
+    text: 'Units',
+    value: 'units',
+  },
+  {
+    icon: 'mdi-office-building-marker',
+    text: 'Locations',
+    value: 'locations',
+  },
+]
+
+const tabWindow = ref('trippings')
+const isDrawerVisible = ref(xs.value ? false : true)
+</script>
+
+<template>
+  <AppLayout :is-with-app-bar-icon="true" @is-drawer-visible="isDrawerVisible = !isDrawerVisible">
+    <template #navigation>
+      <SideNavigation v-model:is-drawer-visible="isDrawerVisible"></SideNavigation>
+    </template>
+
+    <template #content>
+      <v-container fluid>
+        <HeaderPanel
+          :header-items="['Payroll Management', 'Trippings']"
+          header-icon="mdi-highway"
+          headline="Manage employee's trips."
+        ></HeaderPanel>
+
+        <v-tabs v-model="tabWindow" class="mb-5">
+          <v-tab
+            v-for="item in tabsItems"
+            :key="item.value"
+            class="mx-1"
+            :prepend-icon="item.icon"
+            :text="item.text"
+            :value="item.value"
+            min-width="200"
+            color="primary"
+            variant="flat"
+            rounded="lg"
+          ></v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tabWindow">
+          <v-tabs-window-item value="trippings"> </v-tabs-window-item>
+
+          <v-tabs-window-item value="units"> </v-tabs-window-item>
+
+          <v-tabs-window-item value="locations"> </v-tabs-window-item>
+        </v-tabs-window>
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
