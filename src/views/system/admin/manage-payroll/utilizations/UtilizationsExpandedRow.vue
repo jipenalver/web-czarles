ceb1ce8f973c1ea588c04c64cd2ebdd6a94b4bee
jipenalver <jianchris2k14@gmail.com>
2025-08-10 17:55:38 +0800

    feat: add UtilizationsExpandedRow component for displaying utilization details

---
+++ src/views/system/admin/manage-payroll/utilizations/UtilizationsExpandedRow.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { type Utilization } from '@/stores/utilizations'
+import { useDisplay } from 'vuetify'
+
+const props = defineProps<{
+  columnsLength: number
+  itemData: Utilization
+}>()
+
+const { mobile } = useDisplay()
+</script>
+
+<template>
+  <tr>
+    <td :colspan="props.columnsLength" class="py-2">
+      <v-row :class="mobile ? '' : 'px-4'" :no-gutters="!mobile" dense>
+        <v-divider class="my-3" thickness="1"></v-divider>
+      </v-row>
+    </td>
+  </tr>
+</template>
