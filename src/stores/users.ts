243b3f35068d97524390bd406225626fcb8acc43
jipenalver <jianchris2k14@gmail.com>
2025-08-17 23:52:25 +0800

    feat: refactor user mapping logic and enhance getUsers functionality

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -21,16 +21,44 @@ export type AdminUser = {
 
 export const useUsersStore = defineStore('users', () => {
   // States
+  const users = ref<AdminUser[]>([])
   const usersTable = ref<AdminUser[]>([])
   const usersTableTotal = ref(0)
 
   // Reset State
   function $reset() {
+    users.value = []
     usersTable.value = []
     usersTableTotal.value = 0
   }
 
   // Actions
+  function userMap(user: User) {
+    const { id, email, user_metadata, created_at } = user
+
+    return {
+      id,
+      created_at,
+      user_role: user_metadata.user_role,
+      firstname: user_metadata.firstname,
+      middlename: user_metadata.middlename,
+      lastname: user_metadata.lastname,
+      email: email as string,
+      phone: user_metadata.phone,
+      password: user_metadata.password,
+      is_admin: user_metadata.is_admin,
+      avatar: user_metadata.avatar,
+    }
+  }
+
+  async function getUsers() {
+    const { data } = await supabaseAdmin.auth.admin.listUsers()
+
+    const { users: usersData } = data as { users: User[] }
+
+    users.value = usersData.map(userMap)
+  }
+
   async function getUsersTable({ page, itemsPerPage }: TableOptions) {
     const { data } = await supabaseAdmin.auth.admin.listUsers({
       page: page,
@@ -39,23 +67,7 @@ export const useUsersStore = defineStore('users', () => {
 
     const { users, total } = data as { users: User[]; total: number }
 
-    usersTable.value = users.map((user) => {
-      const { id, email, user_metadata, created_at } = user
-
-      return {
-        id,
-        created_at,
-        user_role: user_metadata.user_role,
-        firstname: user_metadata.firstname,
-        middlename: user_metadata.middlename,
-        lastname: user_metadata.lastname,
-        email: email as string,
-        phone: user_metadata.phone,
-        password: user_metadata.password,
-        is_admin: user_metadata.is_admin,
-        avatar: user_metadata.avatar,
-      }
-    })
+    usersTable.value = users.map(userMap)
     usersTableTotal.value = total
   }
 
@@ -83,5 +95,15 @@ export const useUsersStore = defineStore('users', () => {
   }
 
   // Expose States and Actions
-  return { usersTable, usersTableTotal, $reset, getUsersTable, addUser, updateUser, deleteUser }
+  return {
+    users,
+    usersTable,
+    usersTableTotal,
+    $reset,
+    getUsers,
+    getUsersTable,
+    addUser,
+    updateUser,
+    deleteUser,
+  }
 })
