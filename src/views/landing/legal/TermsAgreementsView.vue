eff4b2abbb7263c95c9997f5c65fac25ba683d9a
jipenalver <jianchris2k14@gmail.com>
2025-08-17 22:03:42 +0800

    fix: update route of Privacy Policy and Terms Agreements

---
+++ src/views/landing/legal/TermsAgreementsView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppLayout from '@/components/layout/AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <v-container class="pa-0" fluid>
+        <v-row>
+          <!-- Birahi dri Elro -->
+        </v-row>
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
