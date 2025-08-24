f1989be9dad0c66f8b335cfc4dd9129b34c15270
BritneySpears <centmarde.campado@gmail.com>
2025-08-24 17:59:09 +0800

    refactor: update FooterNavigation component by commenting out unused divider and simplifying copyright text

--- src/components/landing/navigation/FooterNavigation.vue
+++ src/components/landing/navigation/FooterNavigation.vue
@@ -14,16 +14,11 @@ const icons = ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
       ></v-btn>
     </div>
 
-    <v-divider class="my-2" thickness="2" width="50"></v-divider>
+   <!--  <v-divider class="my-2" thickness="2" width="50"></v-divider>
 
-    <div class="text-caption font-weight-regular opacity-60">
-      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo
-      interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa
-      consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-      lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin.
-      Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus.
-    </div>
+   <div class="text-caption font-weight-regular opacity-60">
+  Czarles Construction and Supplies — Building your future with quality and integrity. Contact us for more information about our services.
+</div> -->
 
     <!-- Footer Links -->
     <div class="d-flex ga-4 flex-wrap justify-center">
