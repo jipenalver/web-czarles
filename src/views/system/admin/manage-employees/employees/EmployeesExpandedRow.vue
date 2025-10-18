5b601691b3da9ecb5ad4a1087212837d9d295fe3
jipenalver <jianchris2k14@gmail.com>
2025-10-18 17:06:40 +0800

    fix: update field staff label and add leave reason display in attendance view

--- src/views/system/admin/manage-employees/employees/EmployeesExpandedRow.vue
+++ src/views/system/admin/manage-employees/employees/EmployeesExpandedRow.vue
@@ -78,8 +78,10 @@ const { mobile } = useDisplay()
           class="d-flex align-center my-2"
           :class="mobile ? 'justify-space-between' : 'justify-start'"
         >
-          <span class="text-body-2 font-weight-bold me-2">Is Field Staff?:</span>
-          <p class="text-body-2">{{ props.itemData.is_field_staff ? 'Yes' : 'No' }}</p>
+          <span class="text-body-2 font-weight-bold me-2">Field or Office:</span>
+          <p class="text-body-2">
+            {{ props.itemData.is_field_staff ? 'Field Staff' : 'Office Staff' }}
+          </p>
         </v-col>
 
         <v-col
