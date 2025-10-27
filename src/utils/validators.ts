7f7f52c308876eb969f13ec3c89a339a7df21603
jipenalver <jianchris2k14@gmail.com>
2025-10-27 11:48:21 +0800

    feat: update file size validation to allow up to 5 MB and add image file type validation

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -139,7 +139,27 @@ export const alphaDashValidator = (value: unknown) => {
 export const fileSizeValidator = (value: FileList) => {
   if (isEmpty(value)) return true
 
-  return !value || !value.length || value[0].size < 2000000 || 'File size should be less than 2 MB'
+  return (
+    !value ||
+    !value.length ||
+    value[0].size < 5 * 1024 * 1024 ||
+    `File size should be less than 5 MB`
+  )
+}
+
+// 👉 Image Validator
+export const imageValidator = (value: FileList) => {
+  if (isEmpty(value)) return true
+
+  const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']
+
+  return (
+    !value ||
+    !value.length ||
+    value[0].size < 2 * 1024 * 1024 ||
+    !allowedTypes.includes(value[0].type) ||
+    `Only image files (JPEG, PNG) are allowed. Max size of 2MB.`
+  )
 }
 
 // 👉 General Date Comparison Validator
