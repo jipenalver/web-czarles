297761de2ceea187b071e367ca3c3c9476e7dcc3
jipenalver <jianchris2k14@gmail.com>
2026-01-21 15:14:46 +0800

    feat: implement attendance requests store with CRUD operations and filtering

---
+++ src/stores/attendanceRequests.ts
@@ -0,0 +1,138 @@
+import { type TableOptions, tablePagination } from '@/utils/helpers/tables'
+import { type PostgrestFilterBuilder } from '@supabase/postgrest-js'
+import { prepareDateRange } from '@/utils/helpers/dates'
+import { type Employee } from './employees'
+import { supabase } from '@/utils/supabase'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export type AttendanceRequest = {
+  id: number
+  created_at: string
+  date: string | null
+  employee_id: number | null
+  employee: Employee
+  is_am_leave: boolean
+  is_pm_leave: boolean
+  is_leave_with_pay: boolean
+  leave_type: string | null
+  leave_reason: string
+  leave_status: 'Pending' | 'Approved' | 'Rejected'
+  requestor_id: string
+  user_avatar: string | null
+  user_fullname: string
+}
+
+export type AttendanceRequestTableFilter = {
+  employee_id: number | null
+  attendance_at: Date[] | null
+  component_view: 'leave-requests' | 'overtime-requests'
+}
+
+export const useAttendanceRequestsStore = defineStore('attendanceRequests', () => {
+  const selectQuery = '*, employee:requestor_id (id, firstname, lastname, middlename)'
+
+  // States
+  const attendanceRequests = ref<AttendanceRequest[]>([])
+  const attendanceRequestsTable = ref<AttendanceRequest[]>([])
+  const attendanceRequestsTableTotal = ref(0)
+
+  // Reset State
+  function $reset() {
+    attendanceRequests.value = []
+    attendanceRequestsTable.value = []
+    attendanceRequestsTableTotal.value = 0
+  }
+
+  // Actions
+  async function getAttendanceRequests() {
+    const { data } = await supabase
+      .from('attendance_requests')
+      .select(selectQuery)
+      .order('created_at', { ascending: false })
+
+    attendanceRequests.value = data as AttendanceRequest[]
+  }
+
+  async function getAttendanceRequestsTable(
+    tableOptions: TableOptions,
+    tableFilters: AttendanceRequestTableFilter,
+  ) {
+    const { rangeStart, rangeEnd, column, order } = tablePagination(
+      tableOptions,
+      'created_at',
+      false,
+    )
+
+    let query = supabase
+      .from('attendance_requests')
+      .select(selectQuery)
+      .order(column, { ascending: order })
+      .range(rangeStart, rangeEnd)
+
+    query = getAttendanceRequestsFilter(query, tableFilters)
+
+    const { data } = await query
+
+    const { count } = await getAttendanceRequestsCount(tableFilters)
+
+    attendanceRequestsTable.value = data as AttendanceRequest[]
+    attendanceRequestsTableTotal.value = count as number
+  }
+
+  async function getAttendanceRequestsCount(tableFilters: AttendanceRequestTableFilter) {
+    let query = supabase.from('attendance_requests').select('*', { count: 'exact', head: true })
+
+    query = getAttendanceRequestsFilter(query, tableFilters)
+
+    return await query
+  }
+
+  function getAttendanceRequestsFilter(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    query: PostgrestFilterBuilder<any, any, any, any>,
+    { employee_id, attendance_at, component_view }: AttendanceRequestTableFilter,
+  ) {
+    if (employee_id) query = query.eq('employee_id', employee_id)
+
+    if (attendance_at) {
+      const { startDate, endDate } = prepareDateRange(attendance_at, attendance_at.length > 1)
+
+      if (startDate && endDate) query = query.or(`and(date.gte.${startDate},date.lt.${endDate})`)
+    }
+
+    if (component_view === 'leave-requests') query = query.is('overtime_status', null)
+    else if (component_view === 'overtime-requests') query = query.is('leave_status', null)
+
+    return query
+  }
+
+  async function addAttendanceRequest(formData: Partial<AttendanceRequest>) {
+    return await supabase.from('attendance_requests').insert(formData).select()
+  }
+
+  async function updateAttendanceRequest(formData: Partial<AttendanceRequest>) {
+    return await supabase
+      .from('attendance_requests')
+      .update(formData)
+      .eq('id', formData.id)
+      .select()
+  }
+
+  async function deleteAttendanceRequest(id: number) {
+    return await supabase.from('attendance_requests').delete().eq('id', id).select()
+  }
+
+  // Expose States and Actions
+  return {
+    attendanceRequests,
+    attendanceRequestsTable,
+    attendanceRequestsTableTotal,
+    $reset,
+    getAttendanceRequests,
+    getAttendanceRequestsTable,
+    addAttendanceRequest,
+    updateAttendanceRequest,
+    deleteAttendanceRequest,
+  }
+})
