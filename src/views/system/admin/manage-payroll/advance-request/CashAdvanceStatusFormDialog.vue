acf8c085b9c3a11c4ff4dba0d6ad60fd934dbcaa
jipenalver <jianchris2k14@gmail.com>
2026-02-23 09:34:15 +0800

    feat: add CashAdvanceStatusFormDialog for managing cash advance approvals and rejections

---
+++ src/views/system/admin/manage-payroll/advance-request/CashAdvanceStatusFormDialog.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import {
+  type CashAdvanceRequest,
+  type CashAdvanceRequestTableFilter,
+} from '@/stores/cashAdvanceRequests'
+import { useCashAdvanceStatusFormDialog } from './cashAdvanceStatusFormDialog'
+import { type TableOptions } from '@/utils/helpers/tables'
+import AppAlert from '@/components/common/AppAlert.vue'
+import { requiredValidator } from '@/utils/validators'
+import { useDate, useDisplay } from 'vuetify'
+
+const props = defineProps<{
+  isDialogVisible: boolean
+  itemData: CashAdvanceRequest | null
+  tableOptions: TableOptions
+  tableFilters: CashAdvanceRequestTableFilter
+}>()
+
+const emit = defineEmits(['update:isDialogVisible'])
+
+const { mdAndDown } = useDisplay()
+const date = useDate()
+
+const { formData, formAction, refVForm, onFormSubmit, onFormReset } =
+  useCashAdvanceStatusFormDialog(props, emit)
+</script>
+
+<template>
+  <AppAlert
+    v-model:is-alert-visible="formAction.formAlert"
+    :form-message="formAction.formMessage"
+    :form-status="formAction.formStatus"
+  ></AppAlert>
+
+  <v-dialog
+    :max-width="mdAndDown ? undefined : '600'"
+    :model-value="props.isDialogVisible"
+    :fullscreen="mdAndDown"
+    persistent
+  >
+    <v-card
+      prepend-icon="mdi-thumbs-up-down"
+      title="Cash Advance Request Status"
+      :subtitle="`${props.itemData?.employee.firstname + ' ' + props.itemData?.employee.lastname}: ${date.format(props.itemData?.request_at as string, 'fullDate')}`"
+    >
+      <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12">
+              <v-radio-group
+                v-model="formData.status"
+                label="Status"
+                :rules="[requiredValidator]"
+                inline
+              >
+                <v-radio label="Approve" value="Approved"></v-radio>
+                <v-radio label="Reject" value="Rejected"></v-radio>
+              </v-radio-group>
+            </v-col>
+
+            <v-col v-if="formData.status === 'Rejected'" cols="12">
+              <v-textarea
+                v-model="formData.reason"
+                label="Reason"
+                rows="3"
+                :rules="[requiredValidator]"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="pa-4">
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close" variant="plain" prepend-icon="mdi-close" @click="onFormReset"></v-btn>
+
+          <v-btn
+            prepend-icon="mdi-pencil"
+            color="primary"
+            type="submit"
+            variant="elevated"
+            :disabled="formAction.formProcess"
+            :loading="formAction.formProcess"
+          >
+            Set Status
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
